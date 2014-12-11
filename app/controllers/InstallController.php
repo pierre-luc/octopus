ac94e22bbb3ecb5bff2dc5d7e93f4d1f72723c2d
Pierre-Luc BLOT <pierreluc.blot@gmail.com>
2014-12-11 18:23:50 +0100

    Création du processus d'installation de base de données

--- app/controllers/InstallController.php
+++ app/controllers/InstallController.php
@@ -1,13 +1,13 @@
 <?php
 namespace octopus\app\controllers;
-use octopus\app\Debug;
 use octopus\Config;
 use octopus\core\Controller;
+use octopus\core\DataBaseManager;
 
 class InstallController extends Controller {
+
     public function index() {
         $this->setLayout( 'installer' );
-        $this->sendVariables( 'appname', Config::getAppName() );
     }
 
     public function database() {
@@ -29,6 +29,33 @@ class InstallController extends Controller {
         $f = fopen( APP . DS . 'parameters.json', 'a' );
         fwrite( $f, $r );
         fclose( $f );
+
+        $this->redirect( 'install/databaseInstallation' );
+    }
+
+    public function databaseInstallation() {
+        $this->setLayout( 'installer' );
+    }
+
+    public function databaseInstallationProcess() {
+        Config::loadParameters();
+        $dbname = Config::$databases[ 'default' ][ 'database' ];
+        $f = fopen( APP . DS . $dbname . ".sql", "r");
+        $sql = "";
+        while (($data = fgets($f)) != NULL) {
+            if (substr($data, 0, 2) != "--") {
+                $data = str_replace(chr(10), chr(13), $data);
+                $data = str_replace(chr(13)," ", $data);
+                $sql = $sql.$data;
+            }
+        }
+        fclose($f);
+        $tSql = explode(";", $sql);
+
+        unset($tSql[sizeof($tSql) - 1]);
+        foreach ($tSql as $e) {
+            DataBaseManager::execute($e.';');
+        }
         die();
     }
 }
