81034efab189a0cf63f04c8f492644aa6e419f1c
Pierre-Luc BLOT <pierreluc.blot@gmail.com>
2014-12-12 02:21:35 +0100

    Correction commentaire de doc de getModel

--- core/Controller.php
+++ core/Controller.php
@@ -211,7 +211,7 @@ class Controller {
      * Permet de récupérer un model via son nom.
      * Retourne l'instance du model en question, null s'il n'a pas été chargé.
      * @param $name
-     * @return mixed
+     * @return Model
      */
     public function getModel( $name ) {
         return isset( $this->models[ $name ] ) ? $this->models[ $name ] : null;
